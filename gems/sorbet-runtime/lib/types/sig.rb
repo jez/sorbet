f6282ddac3f24f46b49973af1e192301f36c97f8
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-10 17:40:17 -0700

    Allow sig to take args

--- gems/sorbet-runtime/lib/types/sig.rb
+++ gems/sorbet-runtime/lib/types/sig.rb
@@ -7,19 +7,19 @@ module T::Sig
   module WithoutRuntime
     # At runtime, does nothing, but statically it is treated exactly the same
     # as T::Sig#sig. Only use it in cases where you can't use T::Sig#sig.
-    def self.sig(&blk); end # rubocop:disable PrisonGuard/BanBuiltinMethodOverride
+    def self.sig(*args, &blk); end # rubocop:disable PrisonGuard/BanBuiltinMethodOverride
 
     # At runtime, does nothing, but statically it is treated exactly the same
     # as T::Sig#sig. Only use it in cases where you can't use T::Sig#sig.
-    T::Sig::WithoutRuntime.sig {params(blk: T.proc.bind(T::Private::Methods::DeclBuilder).void).void}
-    def self.sig(&blk); end # rubocop:disable PrisonGuard/BanBuiltinMethodOverride, Lint/DuplicateMethods
+    T::Sig::WithoutRuntime.sig {params(args: Symbol, blk: T.proc.bind(T::Private::Methods::DeclBuilder).void).void}
+    def self.sig(*args, &blk); end # rubocop:disable PrisonGuard/BanBuiltinMethodOverride, Lint/DuplicateMethods
   end
 
   # Declares a method with type signatures and/or
   # abstract/override/... helpers. See the documentation URL on
   # {T::Helpers}
-  T::Sig::WithoutRuntime.sig {params(blk: T.proc.bind(T::Private::Methods::DeclBuilder).void).void}
-  def sig(&blk) # rubocop:disable PrisonGuard/BanBuiltinMethodOverride
-    T::Private::Methods.declare_sig(self, &blk)
+  T::Sig::WithoutRuntime.sig {params(args: Symbol, blk: T.proc.bind(T::Private::Methods::DeclBuilder).void).void}
+  def sig(*args, &blk) # rubocop:disable PrisonGuard/BanBuiltinMethodOverride
+    T::Private::Methods.declare_sig(self, args, &blk)
   end
 end
